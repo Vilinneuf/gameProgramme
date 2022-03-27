d0909fcf4bacbd1d7a4e0c713c514a2dcd26e853
Linli Han(kress) <101640638+Vilinneuf@users.noreply.github.com>
2022-03-27 23:06:38 +0800

    Update 1forList.py

--- 1forList.py
+++ 1forList.py
@@ -12,3 +12,10 @@ list = [1, 3, 5, 7, 9]
 # Using for loop
 for i in list:
 	print(i)
+
+	# Python3 code to iterate over a list
+list = [1, 3, 5, 7, 9]
+
+# Using for loop
+for i in list:
+	print(i)
