58987e88ec124bce1e3c376a38d82c7ac5243a8b
Linli Han(kress) <101640638+Vilinneuf@users.noreply.github.com>
2022-03-27 23:08:15 +0800

    Update 1forList.py

--- 1forList.py
+++ 1forList.py
@@ -8,11 +8,3 @@ for i in list:
 # Python3 code to iterate over a list
 list = [1, 3, 5, 7, 9]
 
-# Using for loop
-for i in list:
-	print(i)# Python3 code to iterate over a list
-list = [1, 3, 5, 7, 9]
-
-# Using for loop
-for i in list:
-	print(i)
