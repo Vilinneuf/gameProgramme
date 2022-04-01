9d7a1857f8beecd5f0562c969618887a0e660fdb
Han Linli <1335324320@qq.com>
2022-04-01 17:04:33 +0800

    Fill the dictionary

--- 5Dictionary2.py
+++ 5Dictionary2.py
@@ -1,7 +1,9 @@
 import random
 
-key = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
-
-
+keys = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
 characterA = {}
+
+for key in keys:
+    characterA[key] = random.randint(1, 20)
+
