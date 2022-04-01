dd913dc653e5c113c698c79eb968c567da64825b
Han Linli <1335324320@qq.com>
2022-04-01 16:38:43 +0800

    finished 4Dictionary.py

--- 4Dictionary.py
+++ 4Dictionary.py
@@ -9,3 +9,13 @@ characterA = {"strength": 12,
 print(characterA)
 for (key, value) in characterA.items():
     print(key, ":", value)
+
+print()
+if "strength" in characterA.keys():
+    print('strength :', characterA["strength"])
+else:
+    print("strength isn't a character stat.")
+if "speed" in characterA.keys():
+    print('speed :', characterA["speed"])
+else:
+    print("speed isn't a character stat.")
