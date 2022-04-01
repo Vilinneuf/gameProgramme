79010d73d7828fdd3f42eaa8462b9e8b92332c9f
Han Linli <1335324320@qq.com>
2022-04-01 15:47:10 +0800

    Create a dictionary

---
+++ 4Dictionary.py
@@ -0,0 +1,8 @@
+
+characterA = {"strength": 12,
+              "dexterity": 10,
+              "constitution": 15,
+              "intelligence": 8,
+              "wisdom": 9,
+              "charisma": 13}
+
