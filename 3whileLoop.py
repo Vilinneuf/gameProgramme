eda38f51e117212e524cd309d8c8dc74014704f2
Han Linli <1335324320@qq.com>
2022-04-01 15:43:23 +0800

    type q qiut

--- 3whileLoop.py
+++ 3whileLoop.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 
 isRunning = True
 while isRunning:
     option = input('Would you like to quit(q) or not? ')
     if option == 'q':
         isRunning = False
-=======
-
->>>>>>> 5c4420139a6bc809e31492d5b2aee565e610a920
