cbcddec6dcc80d48311c9c23bb2822edbd960383
Han Linli <1335324320@qq.com>
2022-04-01 16:30:35 +0800

    type q or Q quit

--- 3whileLoop.py
+++ 3whileLoop.py
@@ -1,6 +1,6 @@
 
 isRunning = True
 while isRunning:
-    option = input('Would you like to quit(q) or not? ')
-    if option == 'q':
+    option = input('Would you like to quit(q or Q) or not? ')
+    if option == 'q' or option == 'Q':
         isRunning = False
