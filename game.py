606b322a1e9035709770fadca6000a5c72e5603b
Han Linli <1335324320@qq.com>
2022-04-24 20:20:56 +0800

    add class item

--- game.py
+++ game.py
@@ -1,3 +1,19 @@
 # Game Item Class with Image
 import random
 import pygame
+
+
+class Item:
+    def __init__(self,filename,screenSize,speed):
+        """Initiate the Item"""
+        if filename:
+            # read the image from the file
+            self.image = pygame.image.load(filename)
+            self.rect = self.image.get_rect()
+        self.screenSize = screenSize
+        self.speed = speed
+        # set the initial position
+        if filename:
+            gap = 10
+            self.rect.x = random.randint(gap, self.screenSize[0] - self.rect.width - gap)
+            self.rect.y = random.randint(gap, self.screenSize[1] - self.rect.height - gap)
